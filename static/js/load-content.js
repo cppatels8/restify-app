c8583d86cef11ef1a3c1756540f95053e824cc00
cppatels8 <cppatels8@gmail.com>
2013-12-30 20:54:14 +0530

    Adding shedule option

--- static/js/load-content.js
+++ static/js/load-content.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             beforeSend: function( xhr ) {
                 if (document.cookie == "")
                 {
-                	window.location.href = "/home";
+                	window.location.href = "/login";
                 }
             },
 		    success : function(data) {
@@ -23,6 +23,49 @@ $(document).ready(function() {
 		    	$('#table1').append(rrr);
 		    		
 		    }
+	    });
+	    $('#scheduleInterview').click(function(){
+	    	var	candidateName = $('.modal-body').find("#candidateName").val(),
+	    	candidateEmail = $('.modal-body').find("#candidateEmail").val();
+	    	round = $('.modal-body').find("#round").val();
+	    	band = $('.modal-body').find("#band").val();
+	    	contactNumber = $('.modal-body').find("#contactNumber").val();
+	    	interviewType = $('.modal-body').find("#interviewType").val();
+	    	interviewTime = $('.modal-body').find("#interviewTime").val();
+	    	interviewerEmail = $('.modal-body').find("#interviewerEmail").val();
+	    	scheduleData =  {
+	    			  "roundNumber": round,
+	    			  "interviewTime": interviewTime,
+	    			  "interviewType": interviewType,
+	    			  "condidateEmailId": candidateEmail,
+	    			  "interviewFeedbackUri": "Url",
+	    			  "band": parseInt(band),
+	    			  "contactNumber": contactNumber,
+	    			  "interviewStatus": "open",
+	    			  "condidateName": candidateName,
+	    			  "interviewerEmailId":interviewerEmail
+	    			};
+	    	$.ajax({
+                url: 'http://restify.labs.hashedin.com/hashedinrecruitmentapp/recruitment?accessToken='+ document.cookie, // php script to retern json encoded string
+                data: JSON.stringify(scheduleData),  // serialized data to send on server
+                dataType: 'json', // set recieving type - JSON in case of a question
+                contentType: 'application/json',
+                type:'POST', // set sending HTTP Request type
+                async:false,
+                crossDomain : true,
+                origin: 'http://restify.labs.hashedin.com',
+                success: function(data) { // callback method for further manipulations             
+                	location.reload();
+                },
+                error: function(data) { // if error occured
+                	alert("Some error ocured. try again later.");
+                }
+              });
+	    
+	    	
+	    	
+	    	
+	    	
 	    });
 
     });
